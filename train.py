fdd0184f122f27ebc77adc90396d17022dbb39a7
arsenalahm <clevercake7@gmail.com>
2019-05-10 10:12:38 +0800

    Modify structure

--- train.py
+++ train.py
@@ -1,12 +1,16 @@
 from utils import *
 import preproc
-import dtree
-import svm
 import random
 import numpy as np
+from dtree import dtree_bagging, dtree_ada_boost
+from svm import svm_bagging, svm_ada_boost
+
+bagging_map = {"dtree": dtree_bagging, "svm": svm_bagging}
+ada_boost_map = {"dtree": dtree_ada_boost, "svm": svm_ada_boost}
 
 
-def dtree_train_bagging():
+def train_bagging(method):
+    classifier = bagging_map[method]
     train_data, train_y, vali_data, vali_y = preproc.load_data()
     test_set = preproc.load_test()
     for i in range(bagging_times):
@@ -17,24 +21,11 @@ def dtree_train_bagging():
             id = int(random.random() * len(train_y))
             bagging_x.append(train_data[id])
             bagging_y.append(train_y[id])
-        dtree.dtree_bagging(bagging_x, bagging_y, vali_data, vali_y, test_set, str(i))
+        classifier(bagging_x, bagging_y, vali_data, vali_y, test_set, str(i))
 
 
-def svm_train_bagging():
-    train_data, train_y, vali_data, vali_y = preproc.load_data()
-    test_set = preproc.load_test()
-    for i in range(bagging_times):
-        print("bagging: ", i)
-        bagging_x = []
-        bagging_y = []
-        for j in range(len(train_y)):
-            id = int(random.random() * len(train_y))
-            bagging_x.append(train_data[id])
-            bagging_y.append(train_y[id])
-        svm.svm_bagging(bagging_x, bagging_y, vali_data, vali_y, test_set, str(i))
-
-
-def dtree_train_ada():
+def train_ada_boost(method):
+    classifier = ada_boost_map[method]
     train_data, train_y, vali_data, vali_y = preproc.load_data()
     test_set = preproc.load_test()
     weights = np.array([1.0 / len(train_data)] * len(train_data))
@@ -42,28 +33,13 @@ def dtree_train_ada():
         indices = np.random.choice(len(train_data), size=len(train_data), p=weights)
         data = np.array(train_data)[indices]
         y = np.array(train_y)[indices]
-        weights, sigma = dtree.dtree_ada_boost(data, y, vali_data, vali_y, test_set, id=str(i), weights=weights, raw_data=train_data, raw_y=train_y)
-        print("Ada_Boost: ", i, sigma)
-        if sigma > 0.5:
-            break
-
-
-def svm_train_ada():
-    train_data, train_y, vali_data, vali_y = preproc.load_data()
-    test_set = preproc.load_test()
-    weights = np.array([1.0 / len(train_data)] * len(train_data))
-    for i in range(ada_times):
-        indices = np.random.choice(len(train_data), size=len(train_data), p=weights)
-        data = np.array(train_data)[indices]
-        y = np.array(train_y)[indices]
-        weights, sigma = svm.svm_ada_boost(data, y, vali_data, vali_y, test_set, id=str(i), weights=weights, raw_data=train_data, raw_y=train_y)
+        weights, sigma = classifier(data, y, vali_data, vali_y, test_set, id=str(i), weights=weights,
+                                               raw_data=train_data, raw_y=train_y)
         print("Ada_Boost: ", i, sigma)
         if sigma > 0.5:
             break
 
 
 if __name__ == "__main__":
-    # dtree_train_bagging()
-    svm_train_bagging()
-    # dtree_train_ada()
-    # svm_train_ada()
+    train_bagging("svm")
+    # train_ada_boost("dtree)
